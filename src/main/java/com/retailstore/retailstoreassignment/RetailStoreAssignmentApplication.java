8d29d56a3df6a1d6490c106afaca7bb2191333b8
İlkay Aktaş <ilkayaktas@gmail.com>
2022-06-02 01:00:36 +0300

    Project is initialized.

---
+++ src/main/java/com/retailstore/retailstoreassignment/RetailStoreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.retailstore.retailstoreassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retailstore/retailstoreassignment/RetailStoreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailstore.retailstoreassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailStoreAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
